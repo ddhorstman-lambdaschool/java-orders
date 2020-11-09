14886e969047dab7b25dc68a311c85d229381e35
David Horstman <davidzilla27@gmail.com>
2020-11-08 16:45:14 -0800

    Add Swagger docs

--- src/main/java/com/davidhorstman/orders/OrdersApplication.java
+++ src/main/java/com/davidhorstman/orders/OrdersApplication.java
@@ -2,12 +2,27 @@ package com.davidhorstman.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class OrdersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrdersApplication.class, args);
+    }
+
+    //https://www.tutorialspoint.com/spring_boot/spring_boot_enabling_swagger2.htm
+    @Bean
+    public Docket apiDocs() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.davidhorstman.orders"))
+                .build();
     }
 
 }
